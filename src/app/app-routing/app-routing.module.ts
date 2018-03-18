005dfe869d2fdb73a09ecec2d60417b980f2724c
neko <floriandancausse@gmail.com>
2018-03-18 21:15:28 +0100

    working route tournament

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from '../home-page/home-page.component';
+import { TournamentComponent } from '../tournament/tournament.component'
 
 const routes: Routes = [
        {
            path: '',
            component: HomePageComponent,
        },
+       {
+         path: 'tournament',
+         component: TournamentComponent,
+       }
    ];
 
 @NgModule({
