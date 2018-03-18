66d3651cd3e060ff08b710861141499a74c0118c
neko <floriandancausse@gmail.com>
2018-03-18 19:08:53 +0100

    setting up routing and sass (not working)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HomePageComponent } from './home-page/home-page.component';
+import { TournamentComponent } from './tournament/tournament.component';
+import { HeadNavComponent } from './head-nav/head-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    TournamentComponent,
+    HeadNavComponent
   ],
   imports: [
     BrowserModule
