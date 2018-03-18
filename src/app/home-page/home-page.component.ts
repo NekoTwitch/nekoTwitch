112e0d8a0a8f56da90da6ed3e4d6f947246b308e
neko <floriandancausse@gmail.com>
2018-03-18 23:55:43 +0100

    embed twitch, import font (working on font import)

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare var $:any;
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor() {
+    document.write("<script src='https://embed.twitch.tv/embed/v1.js'></script><script type='text/javascript'>new Twitch.Embed('twitch-embed', {width: '100%', height: '100%', channel: 'monstercat'});</script>'")
+  }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    console.log($('body'));
   }
 
 }
