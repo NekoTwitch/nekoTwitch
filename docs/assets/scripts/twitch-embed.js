90683514398116afc43b4e356609f74b27886712
neko <floriandancausse@gmail.com>
2018-03-26 00:17:38 +0200

    fix directives + done build

---
+++ docs/assets/scripts/twitch-embed.js
@@ -0,0 +1,8 @@
+new Twitch.Embed("twitch-embed",
+  {
+    theme:"dark",
+    width: "100%",
+    height: "100%",
+    channel: "imfluffyneko"
+   }
+);
--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
-import { ResizeEmbedDirective } from './resize-embed/resize-embed.directive';
 
 @NgModule({
   declarations: [
-    ResizeEmbedDirective
   ],
   exports : [
-    ResizeEmbedDirective
   ]
 })
 export class DirectivesModule { }
