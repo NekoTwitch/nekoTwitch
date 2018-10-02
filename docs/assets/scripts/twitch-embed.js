ad742c586e063cbe7efe35c4324dff01047c18b2
floriandancausse <dancausseflorian@gmail.com>
2018-10-02 23:23:56 +0200

    Update angular_5.2 to angular_6.1

--- docs/assets/scripts/twitch-embed.js
+++
@@ -1,8 +0,0 @@
-new Twitch.Embed("twitch-embed",
-  {
-    theme:"dark",
-    width: "100%",
-    height: "100%",
-    channel: "imfluffyneko"
-   }
-);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 
-import { AppComponent } from './app.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { TournamentComponent } from './pages/tournament/tournament.component';
-import { HeadNavComponent } from './pages/head-nav/head-nav.component';
-import { ScriptHackComponent } from './widgets/script-hack/script-hack.component';
+import {AppComponent} from './app.component';
+import {HomePageComponent} from './pages/home-page/home-page.component';
+import {TournamentComponent} from './pages/tournament/tournament.component';
+import {HeadNavComponent} from './pages/head-nav/head-nav.component';
+import {ScriptHackComponent} from './widgets/script-hack/script-hack.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ScriptHackComponent } from './widgets/script-hack/script-hack.component
     AppRoutingModule,
     BrowserModule
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
